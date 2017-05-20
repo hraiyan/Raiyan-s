bc634f5bb3f996188f1bd218376f254329fb5148
RAIYAN <RAIYAN@Hassan>
2017-05-20 18:45:46 +0530

    Signed-off-by: RAIYAN <RAIYAN@Hassan>

--- src/main/java/ApplicationPages/LogInPage.java
+++ src/main/java/ApplicationPages/LogInPage.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+
 import Factory.DataProviderFactory;
+
+
 
 public class LogInPage {
 	
